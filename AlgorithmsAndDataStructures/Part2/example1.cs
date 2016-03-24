82735ae58b110113540c8dba79a4c7e50558efd5
Erik Tillema <erik@infi.nl>
2016-03-24 16:01:17 +0100

    bugfix

--- AlgorithmsAndDataStructures/Part2/example1.cs
+++ AlgorithmsAndDataStructures/Part2/example1.cs
@@ -1,4 +1,4 @@
-public static double FindMinimum(Func f, double xLowerBound, double xUpperBound, double precision) {
+public static double FindMinimum(Func<double, double> f, double xLowerBound, double xUpperBound, double precision) {
     double xTest = (xUpperBound + xLowerBound) / 2.0;
     if (Math.Abs(xUpperBound - xLowerBound) <= precision) return xTest;
     bool isIncreasing = GetIsIncreasing(f, xTest);
